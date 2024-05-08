a6f77464c9f995c55aefc5c3ab5e5eece2f0fa51
harshbhagtani <harsh.bhagtani1@gmail.com>
2024-05-09 03:20:27 +0530

    socket.io client

--- Providers/SocketProvider/index.tsx
+++ Providers/SocketProvider/index.tsx
@@ -3,10 +3,10 @@ import React, { createContext, useContext, useEffect } from "react";
 import { useState } from "react";
 import { io } from "socket.io-client";
 import { authContext } from "../AuthProvider";
-
+import { BASE_SERVER } from "../../app/constants/config";
 export const socketContext = createContext({});
 
-const socket = io("http://localhost:3002");
+const socket = io(BASE_SERVER);
 
 const SocketProvider = ({ children }: { children: React.ReactNode }) => {
   const { user, isAuthenticated } = useContext(authContext);
